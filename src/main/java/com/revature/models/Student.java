e7a1dfb3d2d39202c0a5904d3a76adb02b9afb94
tlb99 <28433525+tlb99@users.noreply.github.com>
2022-07-15 06:45:41 -0700

    demo done at the last min lol

--- src/main/java/com/revature/models/Student.java
+++ src/main/java/com/revature/models/Student.java
@@ -23,6 +23,13 @@ public class Student {
 
     }
 
+    public Student(int id, String firstName, String lastName, String email) {
+    	this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
